796d25842cb676ff07775a30e8cf9d4af8aca3eb
Tony Maddalone <tjmaddalone@gmail.com>
2020-03-13 07:11:17 -0600

    setup module and demonstrate importability

--- my_bingo/my_script.py
+++ my_bingo/my_script.py
@@ -2,7 +2,17 @@
 
 import pandas
 
+from my_bingo.my_mod import enlarge
+
 print("Hello World")
 
 df = pandas.DataFrame({"state": ["CT", "CO", "AZ", "MT"]})
 print(df.head())
+
+
+print("------------")
+
+
+X = 5
+print("NUMBER", X)
+print("ENLARGED NUMBER", enlarge(X)) # invoking our function
